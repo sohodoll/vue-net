46ee6530284146b3ccc26fc99caea40c598bc043
sohodoll <kovalevvtl@gmail.com>
2023-08-09 17:04:14 +0300

    feat: mouse events and arrays

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
-      title: 'The Final Empire',
-      author: 'Brandon Anderson',
-      age: 34,
+      x: 0,
+      y: 0,
+      books: [
+        { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
+        { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
+        { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true },
+      ],
     }
   },
   methods: {
@@ -13,6 +17,16 @@ const app = Vue.createApp({
     },
     toggleShowBooks() {
       this.showBooks = !this.showBooks
+    },
+    handleEvent(event, data) {
+      console.log(event)
+      if (data) {
+        console.log(data)
+      }
+    },
+    handleMousemove(event) {
+      this.x = event.offsetX
+      this.y = event.offsetY
     },
   },
 })
