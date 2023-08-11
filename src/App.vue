75b58240fc65d257c3b99b508351ca0f0caa6f2d
sohodoll <kovalevvtl@gmail.com>
2023-08-11 11:23:30 +0300

    feat: finish reaction project

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Timer</h1>
+  <button :disabled="isPlaying" @click="start">Play</button>
+  <Block @end="endGame" v-if:="isPlaying" :delay="delay" />
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    }
+  },
   components: {
-    HelloWorld
-  }
+    Block,
+    Results,
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    },
+  },
 }
 </script>
 
@@ -20,7 +44,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template lang="">
+  <p>Reaction time: {{ score }}ms</p>
+  <p class="rank">Your rank is {{ rank }}</p>
+</template>
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank: null,
+    }
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Ninja'
+    } else if (this.score < 500) {
+      this.rank = 'Samurai'
+    } else if (this.score < 1000) {
+      this.rank = 'Warrior'
+    } else {
+      this.rank = 'Beginner'
+    }
+  },
+}
+</script>
+<style>
+.rank {
+  font-size: 2em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
